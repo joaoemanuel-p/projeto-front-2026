520bc69f15470d4b5b06177d07d05521548bffdb
unknown <2514290006@ACADEMICO.IESB.BR>
2026-05-20 09:10:50 -0300

    feat:corrigir pratica07. Fecha #14

--- praticas/aluno-online-react/src/contexts/AuthContext.jsx
+++ praticas/aluno-online-react/src/contexts/AuthContext.jsx
@@ -1,31 +1,3 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
 export const AuthContext = createContext();
-
-export function AuthProvider({ children }) {
-  const [autenticado, setAutenticado] = useState(false);
-  const [usuario, setUsuario] = useState(null);
-
-  function login(dadosUsuario) {
-    setAutenticado(true);
-    setUsuario(dadosUsuario);
-  }
-
-  function logout() {
-    setAutenticado(false);
-    setUsuario(null);
-  }
-
-  return (
-    <AuthContext.Provider
-      value={{
-        autenticado,
-        usuario,
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-}
--- praticas/aluno-online-react/src/contexts/AuthProvider.jsx
+++ praticas/aluno-online-react/src/contexts/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AuthContext } from "./authContext";
+import { AuthContext } from "./AuthContext";
 
 export function AuthProvider({ children }) {
   const [autenticado, setAutenticado] = useState(false);
